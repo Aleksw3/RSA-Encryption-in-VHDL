7d830467ec2703c6a39681c833a1cd589013211d
aleksw3 <aleksander.waage@cern.ch>
2020-10-26 15:27:39 +0100

    Merged versions

--- Components/Compressor/CompressorGeneric.vhd
+++ Components/Compressor/CompressorGeneric.vhd
@@ -1,18 +1,17 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 --Entity
 entity CompressorTree is
 	generic(n : integer := 256);
 	port(
-		A 		: in unsigned(n-1 downto 0);
-		B 		: in unsigned(n-1 downto 0);
-		C 		: in unsigned(n-1 downto 0);
-		D		: in unsigned(n-1 downto 0);
-		Sum 	: out unsigned(n-1 downto 0);
-		Cout	: out unsigned (0 downto 0);
-		Carry 	: out unsigned(n-1 downto 0));
+		A 		: in std_logic_vector(n-1 downto 0);
+		B 		: in std_logic_vector(n-1 downto 0);
+		C 		: in std_logic_vector(n-1 downto 0);
+		D		: in std_logic_vector(n-1 downto 0);
+		Sum 	: out std_logic_vector(n-1 downto 0);
+		Cout	: out std_logic;
+		Carry 	: out std_logic_vector(n-1 downto 0));
 end CompressorTree;
 
 --Architecture
@@ -44,25 +43,19 @@ architecture dataflow of CompressorTree is
 		output2	:	out std_logic);
 	end component;
 	
-	signal A_s, B_s, C_s, D_s : std_logic_vector(n-1 downto 0);
 	signal cinComp, coutComp 		: std_logic_vector(n-1 downto 0);
 	signal outputComp1, outputComp2	: std_logic_vector(n-1 downto 0);
 	
 begin
-	Comp_0 : Compressor42 port map(i(0) => A_s(0), i(1) => B_s(0), i(2) => C_s(0), i(3) => D_s(0),
+	Comp_0 : Compressor42 port map(i(0) => A(0), i(1) => B(0), i(2) => C(0), i(3) => D(0),
 									cin => '0', cout => coutComp(0), output1 => outputComp1(0), output2 => outputComp2(0));
 	Comp_f : for i in 1 to n-1 generate
-		Comp_i : Compressor42 port map(i(0) => A_s(i), i(1) => B_s(i), i(2) => C_s(i), i(3) => D_s(i),
+		Comp_i : Compressor42 port map(i(0) => A(i), i(1) => B(i), i(2) => C(i), i(3) => D(i),
 										cin => coutComp(i-1), cout => coutComp(i), output1 => outputComp1(i), output2 => outputComp2(i));
 	end generate;
-		
-	A_s <= std_logic_vector(A);
-	B_s <= std_logic_vector(B);
-	C_s <= std_logic_vector(C);
-	D_s <= std_logic_vector(D);
-	Sum <= unsigned(outputComp1);
-	Cout <= unsigned(coutComp(n-1 downto n-1));
-	Carry <= unsigned(outputComp2);
 
+	Sum <= outputComp1;
+	Cout <= coutComp(n-1);
+	Carry <= outputComp2;
 	
 end dataflow;
--- RSA_encryption_VHDL/RSA_encryption_VHDL.srcs/sources_1/new/MonPro.vhd
+++ RSA_encryption_VHDL/RSA_encryption_VHDL.srcs/sources_1/new/MonPro.vhd
@@ -26,7 +26,7 @@ use ieee.numeric_std.all;
 entity MonPro is
 	generic(bit_width: integer := 256);
 	port(
-		clk, nrst, EN	:	in std_logic;
+		clk, reset_n, EN	:	in std_logic;
 		N, X, Y 		: 	in std_logic_vector(bit_width - 1 downto 0);
 		busy			:	out std_logic;
 		Z				:	out std_logic_vector(bit_width - 1 downto 0));
@@ -39,14 +39,31 @@ architecture structural of MonPro is
 	component CompressorTree is
 	generic(n : integer := 256);
 	port(
-		A 		: in unsigned(n-1 downto 0);
-		B 		: in unsigned(n-1 downto 0);
-		C 		: in unsigned(n-1 downto 0);
-		D		: in unsigned(n-1 downto 0);
-		Sum 	: out unsigned(n-1 downto 0);
-		Cout	: out unsigned (0 downto 0);
-		Carry 	: out unsigned(n-1 downto 0));
+		A 		: in std_logic_vector(n-1 downto 0);
+		B 		: in std_logic_vector(n-1 downto 0);
+		C 		: in std_logic_vector(n-1 downto 0);
+		D		: in std_logic_vector(n-1 downto 0);
+		Cout	: out std_logic;
+		Sum 	: out std_logic_vector(n-1 downto 0);
+		Carry 	: out std_logic_vector(n-1 downto 0));
 	end Component;
+	
+	component Full_Adder is
+	port(
+		A : in std_logic;
+		B : in std_logic;
+		C_in : in std_logic;
+		S : out std_logic;
+		C_out : out std_logic);
+	end component;
+	
+	component Half_Adder is
+	port(
+		A : in std_logic;
+		B : in std_logic;
+		S : out std_logic;
+		C : out std_logic);
+	end component;
 	
 	type state is (IDLE, INIT, COMPUTE, DONE);
 	signal curr_state, next_state : state;
@@ -55,16 +72,19 @@ architecture structural of MonPro is
 	signal cnt : integer range 0 to bit_width-1 := 0;
 	
 	--Control signals
-	signal done_s : std_logic := '0';
+	signal done_s, initialized, busy_s : std_logic := '0';
 	
 	--Intermediate signals
-	signal Q, SumLSB, Cout : std_logic := '0';
+	signal Q, Cout : std_logic := '0';
 	signal A, B, C, D : std_logic_vector(bit_width-1 downto 0);
 	signal Sum, Carry : std_logic_vector(bit_width-1 downto 0);
 	
 	--Inputs and outputs stored in registers
 	signal X_s, Y_s, N_s, Z_s : std_logic_vector(bit_width-1 downto 0);
 	
+	--CPA input and output signals
+	signal CPA_C, CPA_S : std_logic_vector(bit_width-2 downto 0);
+	signal CPA_A, CPA_B : std_logic_vector(bit_width-2 downto 0);
 	
 	
 	begin
@@ -74,42 +94,69 @@ architecture structural of MonPro is
 					generic map(n => bit_width)
 					port map(A => A, B => B, C => C, D => D,
 					Sum => Sum, Carry => Carry, Cout => Cout);
-					
+					 
 		--Multibit AND operations that are inputs to compressor
 		AndOperations : for i in 0 to bit_width-1 generate
 			C(i) <= N_s(i) and Q;
 			D(i) <= Y_s(i) and X_s(0);
 		end generate;
 		
+		--Generating CPA block for calculating Z
+		HA_RC_0 : Half_Adder port map(A => CPA_A(0), B => CPA_B(0), S => CPA_S(0), C => CPA_C(0));
+	
+		FA_RC_f : for i in 1 to bit_width-2 generate
+			FA_RC_i : Full_Adder port map(A => CPA_A(i), B => CPA_B(i), C_in => CPA_C(i-1), S => CPA_S(i), C_out => CPA_C(i));
+		end generate;
+		
+		--Signal connections to port
+		Z <= Z_s;
+		busy <= busy_s;
 		
 		
 		SynchProc : process(clk)
+			--variable cnt : integer range 0 to bit_width-1 := 0;
 		begin
 			if rising_edge(clk) then	
-				if nrst = '0' then
-					curr_state <= idle;
+				if reset_n = '0' then
+					curr_state <= IDLE;
+					X_s <= (others => '0');
+					Y_s <= (others => '0');
+					N_s <= (others => '0');
+					Z_s <= (others => '0');
 				else
 					--If EN is high, perform Monpro, otherwise IDLE
 					if EN = '1' then
+					
 						--Store input vectors in registers
 						if curr_state = INIT then
 							X_s <= X;
 							Y_s <= Y;
 							N_S <= N;
+							Z_s <= (others => '0');
+							initialized <= '1';
+							
 						--Compute the MonPro: Compressor, shift, store carry and save in registers.
 						elsif curr_state = COMPUTE then
+							--Check whether computation is done
 							if cnt = bit_width - 1 then
-								cnt <= 0;
 								done_s <= '1';
+								cnt <= 0;
+							--Computation is still being performed
 							else
-								A <= 0 & Carry(bit_width-1 downto 1);
-								B <= 0 & Sum(bit_width-1 downto 1);
-								X_s <= shift_right(unsigned(X_s), 1);
+								--Update Compressor input registers with new values
+								--1 bit Right shift inputs to registers A, B and X_s
+								A <= '0' & Carry(bit_width-2 downto 0);
+								B <= '0' & Sum(bit_width-2 downto 0);
+								X_s <= '0' & X_s(bit_width-1 downto 1);
+								Q <= Sum(0) xor D(0);
+								--Update counter
 								cnt <= cnt + 1;
 							end if;
 						--When finished computing, perform CPA and output to Z
 						elsif curr_state = DONE then
-						
+							--Throw away carry and sumMSB of CPA and store result in Z
+							Z_s <= CPA_S(bit_width-2 downto 0) & Sum(0);
+							busy_s <= '0';
 						end if;
 						curr_state <= next_state;
 					else
@@ -119,37 +166,52 @@ architecture structural of MonPro is
 			end if;
 		end process;
 		
+		
 		CombProc : process(clk)
 		begin
 			case(curr_state) is
 				when IDLE =>
+					--Reset signals in case of undesired values
 					Z <= (others => '0');
-					busy <= '0';
-					done <= '0';
+					busy_s <= '0';
+					done_s <= '0';
+					cnt <= 0;
+					initialized <= '0';
 					if EN = '1' then
 						next_state <= INIT;
 					else
 						next_state <= IDLE;
 					end if;
 				when INIT =>
-					busy <= '1';
-					next_state <= COMPUTE;
+					busy_s <= '1';
+					if initialized = '1' then
+						next_state <= COMPUTE;
+					else
+						next_state <= INIT;
+					end if;
 				when COMPUTE =>
+					--Computation finished
 					if done_s = '1' then
 						next_state <= DONE;
+					--Computation still being performed
 					else
-						SumLSB <= Sum(0);
+						next_state <= COMPUTE;
 					end if;
 				when DONE =>
-					
+					--CPA finished and output ready
+					if busy_s = '0' then
+						next_state <= IDLE;
+					--CPA computation still being performed
+					else
+						--CPA inputs of bit_width - 1 bits
+						CPA_A <= Carry(bit_width - 2 downto 0); 
+						CPA_B <= Sum(bit_width - 2 downto 0);
+						next_state <= DONE;
+					end if;
 				when others =>
 					next_state <= IDLE;
 				end case;
 					
 		end process;
-		
-		
-		
-		
-	
+			
 	end architecture;
