73bd76b22d62f5b4b24c0870ee13c0520ce15020
R0cinant3 <sindre-jf@hotmail.com>
2020-11-10 09:48:18 +0100

    Updated MonPro using Kogge-Stone Adders

--- RSA_encryption_VHDL/RSA_encryption_VHDL.srcs/sources_1/new/MonPro.vhd
+++ RSA_encryption_VHDL/RSA_encryption_VHDL.srcs/sources_1/new/MonPro.vhd
@@ -36,124 +36,137 @@ architecture structural of MonPro is
 
 	--Structural components
 	
-	component CompressorTree is
-	generic(n : integer := 256);
-	port(
-		A 		: in std_logic_vector(n-1 downto 0);
-		B 		: in std_logic_vector(n-1 downto 0);
-		C 		: in std_logic_vector(n-1 downto 0);
-		D		: in std_logic_vector(n-1 downto 0);
-		Cout	: out std_logic;
-		Sum 	: out std_logic_vector(n-1 downto 0);
-		Carry 	: out std_logic_vector(n-1 downto 0));
-	end Component;
+	component CompressorMultiBit_4to2 is
+	generic(bit_width : integer);
+	port( 
+		   A 		        : in std_logic_vector(bit_width-1 downto 0);
+		   B 		        : in std_logic_vector(bit_width-1 downto 0);
+		   C 		        : in std_logic_vector(bit_width-1 downto 0);
+		   D		        : in std_logic_vector(bit_width-1 downto 0);
+		   Sum 	            : out std_logic_vector(bit_width-1 downto 0);
+		   Cout	            : out std_logic;
+		   Carry 	        : out std_logic_vector(bit_width-1 downto 0));
+    end component;
 	
-	component CarryPropagateAdder is
-	generic(n : integer := 256);
-	port(
-			A 			: in std_logic_vector(n-1 downto 0);
-			B 			: in std_logic_vector(n-1 downto 0);
-			Sum 		: out std_logic_vector(n-1 downto 0);
-			Cout 		: out std_logic);
-	end component;
+	component KoggeStoneAdder is
+    Port (i_x, i_y : in std_logic_vector(bit_width-1 downto 0);
+        o_result   : out std_logic_vector(bit_width downto 0));
+    end component;
+    
+--    component KoggeStoneAdder32Bit is
+--    Port (i_x, i_y : in std_logic_vector(31 downto 0);
+--        o_result   : out std_logic_vector(32 downto 0));
+--    end component;
 	
 	type state is (INIT, COMPUTE);
 	signal curr_state, next_state : state;
 	
-	--Counter for computing Z
---	signal cnt : integer range 0 to bit_width-1 := 0;
+	--Control Logic
+    signal done_s : std_logic       := '0';
+    signal busy_s : std_logic       := '0';
+
+    --Intermediate Signals
+    signal isODD                    : std_logic;
+    signal compressor_cout          : std_logic;
+    signal C, D                     : std_logic_vector(bit_width-1 downto 0);
+    signal compressor_sum           : std_logic_vector(bit_width-1 downto 0) := (others => '0');
+    signal compressor_carry         : std_logic_vector(bit_width-1 downto 0) := (others => '0');
+    signal compressor_sum_shifted   : std_logic_vector(bit_width-1 downto 0);
+    signal Z_s                      : std_logic_vector(bit_width - 1 downto 0) := (others => '0');
 	
-	--Control signals
-	signal done_s, busy_s : std_logic := '0';
+    --Registers
+    signal X_r, Y_r, N_r, Z_r       : std_logic_vector(bit_width - 1 downto 0);
+    signal A_r, B_r                 : std_logic_vector(bit_width - 1 downto 0);
+    signal KSA_sum_input_r          : std_logic_vector(bit_width - 1 downto 0);
+    signal KSA_carry_input_r        : std_logic_vector(bit_width - 1 downto 0);
+    
+    --CarryLookAheadAdder Signals
+    signal KSA_carry_input          : std_logic_vector(bit_width-1 downto 0) := (others => '0');
+    signal KSA_sum_input            : std_logic_vector(bit_width-1 downto 0) := (others => '0');
+    signal KSA_result               : std_logic_vector(bit_width downto 0) := (others => '0');
 	
-	--Intermediate signals
-	signal Q : std_logic; 
-	signal Cprs_Cout : std_logic;
-	signal A, B, C, D : std_logic_vector(bit_width-1 downto 0);
-	signal Cprs_Carry : std_logic_vector(bit_width-1 downto 0) := (others => '0');
-	signal Cprs_Sum : std_logic_vector(bit_width - 1 downto 0) := (others => '0');
-	signal Cprs_Sum_s : std_logic_vector(bit_width-1 downto 0) := (others => '0'); --shifted signal
-	signal Sum : std_logic_vector(bit_width-1 downto 0);
-	
-	--Inputs and outputs stored in registers
-	signal X_s, Y_s, N_s : std_logic_vector(bit_width-1 downto 0);
-	signal Z_s : std_logic_vector(bit_width-1 downto 0) := (others => '0');
-	
-	--CPA input and output signals
-	signal CPA_Sum : std_logic_vector(bit_width-1 downto 0);
-	signal CPA_B : std_logic_vector(bit_width-1 downto 0);
-	signal CPA_Cout : std_logic;
-	
+	--Counter
+    signal count                    : integer range 0 to bit_width-1 := 0;
 	
 	begin
 		
 		--Component mapping
-		Cprs : CompressorTree 
-					generic map(n => bit_width)
-					port map(A => A, B => B, C => C, D => D,
-					Sum => Cprs_Sum, Carry => Cprs_Carry, Cout => Cprs_Cout);
+		Cprs_Inst : CompressorMultiBit_4to2 
+            generic map(bit_width => bit_width)
+            port map(A => A_r, B => B_r, 
+            C => C, D => D, 
+            Sum => Compressor_Sum, 
+            Carry => Compressor_Carry, 
+            Cout => Compressor_Cout);
 					 
 		--Multibit AND operations that are inputs to compressor
 		AndOperations : for i in 0 to bit_width-1 generate
-			C(i) <= N_s(i) AND Q;
-			D(i) <= Y_s(i) AND X_s(0);
+			C(i) <= N_r(i) AND isODD;
+			D(i) <= Y_r(i) AND X_r(0);
 		end generate;
 		
-		Summation	: CarryPropagateAdder 
-					generic map(n => bit_width)
-					port map(A => Cprs_Carry, B => Cprs_Sum_s, Sum => Sum,
-					Cout => CPA_Cout);
+		KSA_Inst : KoggeStoneAdder
+        port map(i_x => KSA_Carry_Input, 
+        i_y => KSA_Sum_Input, o_result => KSA_Result);
+        
+--        KSA_Inst : KoggeStoneAdder32Bit
+--            port map(i_x => KSA_Carry_Input, 
+--            i_y => KSA_Sum_Input, o_result => KSA_Result);
 	
-	    Q <= B(0) XOR (X_s(0) AND Y_s(0));
-		Cprs_Sum_s <= std_logic_vector(shift_right(unsigned(Cprs_Sum),1));
+	
+	
+	    isODD <= B_r(0) XOR (X_r(0) AND Y_r(0));
+		Compressor_Sum_Shifted <= std_logic_vector(shift_right(unsigned(Compressor_Sum),1));
 		--Signal connections to port
-		Z <= Z_s;
+		Z <= Z_r;
 		busy <= busy_s;
-		
+		KSA_Carry_Input <= KSA_Carry_Input_r;
+		KSA_Sum_Input	<= KSA_Sum_Input_r;
 		
 		SynchProc : process(clk)
-			variable cnt : integer range 0 to bit_width-1 := 0;
+--			variable cnt : integer range 0 to bit_width-1 := 0;
 		begin
 			if rising_edge(clk) then	
-				if reset_n = '0' then
+				if reset_n = '0' then   
 					curr_state <= INIT;
-					X_s <= (others => '0');
-					Y_s <= (others => '0');
-					N_s <= (others => '0');
+					X_r <= (others => '0');
+					Y_r <= (others => '0');
+					N_r <= (others => '0');
 				else
 					--If EN is high, perform Monpro, otherwise IDLE
 					if EN = '1' then
 						case(curr_state) is
 						when INIT =>
 							--Store inputs in registers
-							cnt := 0;
-							X_s <= X;
-                            Y_s <= Y;
-                            N_S <= N;
+							count       <= 0;
+							X_r <= X;
+                            Y_r <= Y;
+                            N_r <= N;
 							--Reset output and intermediate registers
-							A <= (others => '0');
-							B <= (others => '0');
-							Z_s <= (others => '0');
+							A_r <= (others => '0');
+							B_r <= (others => '0');
+							Z_r <= (others => '0');
+							KSA_Sum_Input_r      <= (others => '0');
+							KSA_Carry_Input_r    <= (others => '0');
 							--Set busy_s bit high, indicating computation ongoing
 							done_s <= '0';
 							busy_s <= '1';
 						when COMPUTE =>
-						    if (cnt < bit_width-1) then
-								
-								A <= Cprs_Carry;
-								B <= Cprs_Sum_s;
-								
-								X_s <=  std_logic_vector(shift_right(unsigned(X_s),1));
-								cnt := cnt + 1;
-								if(cnt = bit_width-1) then
-								    done_s <= '1';
-								else
-								    done_s <= '0';
-								end if;
+						    KSA_Sum_Input_r     <= Compressor_Sum_Shifted;
+                            KSA_Carry_Input_r   <= Compressor_Carry;
+						    if (count < bit_width-1) then
+								count <= count + 1;
+								A_r <= Compressor_Carry;
+								B_r <= Compressor_Sum_Shifted;
+								X_r <=  std_logic_vector(shift_right(unsigned(X_r),1));
 							else
-							    Z_s <= Sum;
-							    busy_s <= '0';
-								cnt := 0;
+							    if done_s = '1' then
+							        count   <= 0;
+							        Z_r <= KSA_Result(bit_width - 1 downto 0);
+							        busy_s <= '0';
+							    else
+							        done_s <= '1';
+							    end if;
 							end if;
 						end case;
 						curr_state <= next_state;
