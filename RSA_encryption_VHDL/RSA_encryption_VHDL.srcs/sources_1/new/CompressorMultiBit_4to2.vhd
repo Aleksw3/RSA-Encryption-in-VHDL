d659f3744929117af5f68f2e35e3ea6b7865dfc4
R0cinant3 <sindre-jf@hotmail.com>
2020-11-11 12:30:03 +0100

    Fixed Compressor overflow. Monpro gives the correct value at high input values now

--- RSA_encryption_VHDL/RSA_encryption_VHDL.srcs/sources_1/new/CompressorMultiBit_4to2.vhd
+++ RSA_encryption_VHDL/RSA_encryption_VHDL.srcs/sources_1/new/CompressorMultiBit_4to2.vhd
@@ -5,13 +5,13 @@ use ieee.std_logic_1164.all;
 entity CompressorMultiBit_4to2 is
 	generic(bit_width : natural);
 	port(
-		A 		: in std_logic_vector(bit_width-1 downto 0);
-		B 		: in std_logic_vector(bit_width-1 downto 0);
-		C 		: in std_logic_vector(bit_width-1 downto 0);
-		D		: in std_logic_vector(bit_width-1 downto 0);
-		Sum 	: out std_logic_vector(bit_width-1 downto 0);
+		A 		: in std_logic_vector(bit_width downto 0);
+		B 		: in std_logic_vector(bit_width downto 0);
+		C 		: in std_logic_vector(bit_width downto 0);
+		D		: in std_logic_vector(bit_width downto 0);
+		Sum 	: out std_logic_vector(bit_width downto 0);
 		Cout	: out std_logic;
-		Carry 	: out std_logic_vector(bit_width-1 downto 0));
+		Carry 	: out std_logic_vector(bit_width downto 0));
 end CompressorMultiBit_4to2;
 
 --Architecture
@@ -26,19 +26,19 @@ architecture dataflow of CompressorMultiBit_4to2 is
 		Carry	    :	out std_logic);
 	end component;
 	
-	signal cinComp, coutComp 		: std_logic_vector(bit_width-1 downto 0);
-	signal outputComp1, outputComp2	: std_logic_vector(bit_width-1 downto 0);
+	signal cinComp, coutComp 		: std_logic_vector(bit_width downto 0);
+	signal outputComp1, outputComp2	: std_logic_vector(bit_width downto 0);
 	
 begin
 	Comp_inst : CompressorSimple_4to2 port map(Input(0) => A(0), Input(1) => B(0), Input(2) => C(0), Input(3) => D(0),
 									cin => '0', cout => coutComp(0), Sum => outputComp1(0), Carry => outputComp2(0));
-	CompGen : for i in 1 to bit_width-1 generate
+	CompGen : for i in 1 to bit_width generate
 		Comp_inst : CompressorSimple_4to2 port map(Input(0) => A(i), Input(1) => B(i), Input(2) => C(i), Input(3) => D(i),
 										cin => coutComp(i-1), cout => coutComp(i), Sum => outputComp1(i), Carry => outputComp2(i));
 	end generate;
 
 	Sum <= outputComp1;
-	Cout <= coutComp(bit_width-1);
+	Cout <= coutComp(bit_width);
 	Carry <= outputComp2;
 	
 end dataflow;
